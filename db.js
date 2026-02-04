89ebc39bb0c8cc0e6edd29931b68d7d1359798de
Muskan Kesharwani <muskankesharwani@sigma.in>
2026-02-04 23:48:45 +0530

    Added userSchema

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 //Define the mongoose connection URL
-const mongoURL = process.env.MONGODB_URL_LOCAL
+//const mongoURL = process.env.MONGODB_URL_LOCAL
+const mongoURL = process.env.MONGODB_URL
 
 //set up mongodb connection
 mongoose.connect(mongoURL);
